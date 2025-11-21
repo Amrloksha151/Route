ec0dc5f11475d52563d78421876120787db08a05
Amr Loksha <amrloksha151@proton.me>
2025-11-21 10:52:32 +0200

    updating main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
 from sys import argv
+from .parser import parser
 
 def main():
-    pass
+    if len(argv) > 1:
+        filename = argv[1]
+        with open(filename, 'r') as file:
+            data = file.read()
+        result = parser.parse(data)
+        print(result)
+    else:
+        print("Route Interactive Parser V1.0. Type your commands below. To exit, press Ctrl+D.")
+        while True:
+            try:
+                s = input('route> ')
+            except EOFError:
+                break
+            result = parser.parse(s)
+            print(result)
 
 if __name__ == "__main__":
     main()
